d2a0209d2619f8b21c982bafde01dc09376974f5
AJB1000 <venteslbc1000@gmail.com>
2025-10-27 16:39:48 +0100

    Mise à jour lun. 27 oct. 2025 16:39:48 CET

--- sw.js
+++ sw.js
@@ -1,7 +1,43 @@
+const CACHE_NAME = 'pwa-params-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Installation - mise en cache
 self.addEventListener('install', event => {
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache ouvert');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
+// Interception des requêtes
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Retourne le cache ou fetch en réseau
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Nettoyage des anciens caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
